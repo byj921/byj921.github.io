559a5b1b0e688450bce429e058cd79344a644076
byj921 <57886673+byj921@users.noreply.github.com>
2020-02-27 00:00:16 +0900

    Update main.js

--- vertium/js/main.js
+++ vertium/js/main.js
@@ -3,13 +3,16 @@ $(document).ready(function () {
     $('.btnOpen').click(function () {
         $('#quickMenu').css('top', '40px');
         $('.btnOpen').css('display', 'none')
-    })
+    });
+    
     $('.btnClose').click(function () {
         $('#quickMenu').css('top', '-691px');
         $('.btnOpen').css('display', 'block')
     });
+    
+});
 
-    $('.more_btn').click(function () {
+ $('.more_btn').click(function () {
         $('.con1_box').css('height', '1920px');
         $('.more_btn').css('display', 'none');
     });
@@ -17,6 +20,7 @@ $(document).ready(function () {
     $('.toggle_btn').click(function () {
         $('.toggle').css('right','0%');
     });
+    
     $('.tog_close').click(function () {
         $('.toggle').css('right','-100%');
     });
@@ -28,7 +32,6 @@ $(document).ready(function () {
     $('.fam_tit').click(function () {
         $('.ft_sub').toggle('opacity','1');
     });
-    
-});
+
 
 
